e0c2e85cef5efd2cbff925cda05d76666a88fb8c
Marek Minor <minor.marek@gmail.com>
2018-03-22 08:41:04 +0100

    Supporting more parameters from JSON

--- src/run.js
+++ src/run.js
@@ -1,43 +1,64 @@
 const sketch = require('sketch')
 var Artboard = require('sketch/dom').Artboard
+var Document = require('sketch/dom').Document
 var Group = require('sketch/dom').Group
 var Rectangle = require('sketch/dom').Rectangle
 var Shape = require('sketch/dom').Shape
 var Style = require('sketch/dom').Style
 var Text = require('sketch/dom').Text
+var UI = require('sketch/ui')
+var Settings = require('sketch/settings')
 var SymbolMaster = require('sketch/dom').SymbolMaster
 
+// log(generateUniqueId())
+
 var systems = {
-  "yxf8pjwlhx": {
-    "name": "button",
-    "children": {
-      "o4qrk2kkq4": {
-        "font": "SF Pro Text",
-        "fontColor": "#000000",
-        "fontSize": 14,
-        "fontWeight": "Bold",
-        "index": 1,
-        "name": "label",
-        "text": "Label",
-        "textAlign": "left",
-        "type": "text",
-        "x": 10,
-        "y": 10,
-      },
-      "urx7bp2v24i": {
-        "borderColor": "#1C85FF",
-        "borderRadius": [4, 4, 4, 4],
-        "borderWidth": 0,
-        "fillColor": "#1C85FF",
-        "fillType": "color",
-        "height": 32,
-        "index": 0,
-        "name": "background",
-        "opacity": 0.0,
-        "type": "shape",
-        "width": 120,
-        "x": 0,
-        "y": 0,
+  "fonts": {
+    "mnlmki4s2jr": {
+      "name": "SF Pro Text",
+      "value": "SF Pro Text",
+    }
+  },
+  "colors": {
+    "wrgze8ypmp": {
+      "name": "white",
+      "value": "#000000",
+    }
+  },
+  "custom": {
+    "yxf8pjwlhx": {
+      "name": "button",
+      "children": {
+        "o4qrk2kkq4": {
+          "fixedWidth": false,
+          "font": "SF Pro Text",
+          "fontColor": "#000000",
+          "fontSize": 14,
+          "fontWeight": "Bold",
+          "index": 1,
+          "lineHeight": 100,
+          "name": "label",
+          "text": "Label",
+          "textAlign": "left",
+          "type": "text",
+          "x": 10,
+          "y": 10,
+        },
+        "urx7bp2v24i": {
+          "borderColor": "#1C85FF",
+          "borderRadius": [4, 4, 4, 4],
+          "borderWidth": 0,
+          "fillColor": "#1C85FF",
+          "fillType": "color",
+          "height": 32,
+          "index": 0,
+          "name": "background",
+          "opacity": 0.0,
+          "type": "shape",
+          "width": 120,
+          "x": 0,
+          "y": 0,
+        }
       }
     }
   }
@@ -56,9 +77,9 @@ export default function(context) {
   page.layers = {}
 
   // For every system
-  for (var s in systems) {
+  for (var s in systems.custom) {
 
-    var system = systems[s]
+    var system = systems.custom[s]
     var subsystems = system.children
 
     // Create symbol for system
@@ -71,55 +92,77 @@ export default function(context) {
     for (var ss in subsystems) {
       var subsystem = subsystems[ss]
 
-      // If subsystem is Text
+      // –––––––––––––– TEXT –––––––––––––– //
+
       if (subsystem.type == 'text') {
 
-        // Create label
+        var subsystem_name = subsystem.name
+        var subsystem_fixedWidth = subsystem.fixedWidth
+        var subsystem_font = subsystem.font
+        var subsystem_fontSize = subsystem.fontSize
+        var subsystem_fontWeight = subsystem.fontWeight
+        var subsystem_lineHeight = subsystem.lineHeight
+        var subsystem_text = subsystem.text
+        var subsystem_alignment = subsystem.textAlign
+        var subsystem_x = subsystem.x
+        var subsystem_y = subsystem.y
+
         var text = new Text({
-          text: subsystem.text,
-          name: subsystem.name,
-          parent: artboard,
+          alignment: subsystem_alignment,
+          fixedWidth: subsystem_fixedWidth,
           frame: {
-            x: subsystem.x,
-            y: subsystem.y
-          }
+            x: subsystem_x,
+            y: subsystem_y
+          },
+          lineSpacing: "variable",
+          name: subsystem_name,
+          parent: artboard,
+          text: subsystem_text,
         })
 
-        changeTextColor(text, 1, 1, 1, 1)
+        changeTextColor(text, 0, 0, 1, 1)
 
-        changeTextFont(text, subsystem.font + " " + subsystem.fontWeight, subsystem.fontSize)
+        changeTextFont(text, subsystem_font + " " + subsystem_fontWeight, subsystem_fontSize)
 
-      // Or if subsystem is Shape
+        // log(text.sketchObject.style().textStyle())
+
+      // –––––––––––––– SHAPE –––––––––––––– //
+
       } else if (subsystem.type == 'shape') {
 
-        // Create background
+        var subsystem_borderColor = subsystem.borderColor
+        var subsystem_borderWidth = subsystem.borderWidth
+        var subsystem_borderRadius = subsystem.borderRadius
+        var subsystem_fillColor = subsystem.fillColor
+        var subsystem_name = subsystem.name
+        var subsystem_x = subsystem.x
+        var subsystem_y = subsystem.y
+
         var shape = new Shape({
-          name: subsystem.name,
-          parent: artboard,
           frame: {
-            x: subsystem.x,
-            y: subsystem.y,
+            x: subsystem_x,
+            y: subsystem_y,
             width: text.frame.width + 20,
             height: text.frame.height + 20
           },
+          name: subsystem_name,
+          parent: artboard,
           style: {
-            fills: [{
-              color: subsystem.fillColor,
-              fillType: Style.FillType.color,
-            }],
             borders: [{
-              color: subsystem.borderColor,
+              color: subsystem_borderColor,
               fillType: Style.FillType.color,
-              thickness: subsystem.borderWidth.toString()
+              thickness: subsystem_borderWidth.toString()
             }],
+            fills: [{
+              color: subsystem_fillColor,
+              fillType: Style.FillType.color,
+            }]
           }
         })
 
-        changeRectangleRadius(shape, subsystem.borderRadius[0], subsystem.borderRadius[1], subsystem.borderRadius[2], subsystem.borderRadius[3])
+        changeRectangleRadius(shape, subsystem_borderRadius[0], subsystem_borderRadius[1], subsystem_borderRadius[2], subsystem_borderRadius[3])
 
-        // shape.moveBackward()
-
-        log(shape.sketchObject.index)
+        shape.moveBackward()
 
       }
     }
@@ -129,7 +172,7 @@ export default function(context) {
 
 }
 
-function changeTextColor(text, r, g, b, a) {
+function changeTextColor(text, h, s, l, a) {
 
   // access the underlying sketch object and get its textstyle
   var textStyle = text.sketchObject.style().textStyle()
@@ -138,7 +181,7 @@ function changeTextColor(text, r, g, b, a) {
   var mutableAttributes = NSMutableDictionary.dictionaryWithDictionary(textStyle.attributes())
 
   // set the NSColor key for the attributes dictionary
-  mutableAttributes.setObject_forKey(NSColor.colorWithRed_green_blue_alpha(r,g,b,a),'NSColor');
+  mutableAttributes.setObject_forKey(NSColor.colorWithHue_saturation_brightness_alpha(h,s,l,a),'NSColor');
 
   // write the attributes dictionary back onto the textstyle (and we're done)
   textStyle.setValue_forKey_(mutableAttributes,'attributes');
